d2376539ffa5baa9807a237ad500f3ed3c9b90ec
Algyu <alexandragabrielasalas@gmail.com>
2025-08-20 15:20:44 -0500

    Ejercicio4.java

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,12 +1,15 @@
-public class Ejercicio3 {
-    public static double menor(double a, double b, double c) {
-        double min = a;
-        if (b < min) min = b;
-        if (c < min) min = c;
-        return min;
+public class Ejercicio4 {
+    public static double menor(double num1, double num2, double num3) {
+        if (num1 < num2 && num1 < num3) {
+            return num1;
+        } else if (num2 < num1 && num2 < num3) {
+            return num2;
+        } else {
+            return num3;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("El menor es: " + menor(5.5, 2.3, 9.8));
+        System.out.println("El menor de los tres números es: " + menor(5.5, 2.8, 9.1));
     }
 }
