10ba4ed8b4a1d2b1091ce1952ac7e341d359279b
Algyu <alexandragabrielasalas@gmail.com>
2025-08-20 15:14:53 -0500

    Update Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -3,10 +3,10 @@ import java.util.Random;
 public class Ejercicio2 {
     public static void main(String[] args) {
         Random rand = new Random();
-        int[] frecuencia = new int[6]; // posiciones 0 a 5 representan caras 1 a 6
+        int[] frecuencia = new int[6];
 
         for (int i = 0; i < 20000; i++) {
-            int dado = rand.nextInt(6) + 1; // número entre 1 y 6
+            int dado = rand.nextInt(6) + 1;
             frecuencia[dado - 1]++;
         }
 
