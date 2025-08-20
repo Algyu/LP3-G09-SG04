8963c369e750fff674c39d24dd1e1ba83194f33e
Algyu <alexandragabrielasalas@gmail.com>
2025-08-20 15:19:42 -0500

    Ejercicio5.java

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,21 +1,23 @@
-public class Ejercicio4 {
-    public static double calcularCargo(int horas) {
-        if (horas <= 0) return 0;
-
-        double total = 3.0; // primera hora
+public class Ejercicio5 {
+    public static double calcularCostoEstacionamiento(int horas) {
+        if (horas <= 0) {
+            return 0.0;
+        }
+        double costo = 3.0;
         if (horas > 1) {
-            total += (horas - 1) * 0.5;
+            costo += (horas - 1) * 0.50;
         }
-
-        // máximo 12 soles
-        if (total > 12.0) {
-            total = 12.0;
+        if (costo > 12.0) {
+            return 12.0;
+        } else {
+            return costo;
         }
-        return total;
     }
 
     public static void main(String[] args) {
-        int horas = 10;
-        System.out.println("Por " + horas + " horas, el cargo es: S/" + calcularCargo(horas));
+        int horas1 = 2;
+        System.out.println("El costo por " + horas1 + " horas es: S/" + calcularCostoEstacionamiento(horas1));
+        int horas2 = 24;
+        System.out.println("El costo por " + horas2 + " horas es: S/" + calcularCostoEstacionamiento(horas2));
     }
 }
